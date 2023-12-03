b9dc55a0a725ca6037bd215c70585632f4c50127
Mateus Gabardo Lemos <gabardomateus@gmail.com>
2023-12-03 00:39:29 -0300

    Update CrawlerScheduler.java

--- src/main/java/web/crawler/scheduler/service/CrawlerScheduler.java
+++ src/main/java/web/crawler/scheduler/service/CrawlerScheduler.java
@@ -14,7 +14,7 @@ import java.time.LocalDate;
 @Component
 public class CrawlerScheduler {
     private final String cron = "0 */2 * * * *";
-    private final String urlCrawler = "http://localhost:8080/catholicLyrics/startCrawlAll";
+    private final String urlCrawler = "http://web-crawler-api:8080/catholicLyrics/startCrawlAll";
 
 
     @Scheduled(cron = cron)
